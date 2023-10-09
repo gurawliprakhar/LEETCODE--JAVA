b9b515151a4e87ad0fdd2fbdfc99747190e2a72f
Prakhar Tripathi <110407222+gurawliprakhar@users.noreply.github.com>
2023-10-09 14:15:51 +0530

    update

---
+++ TreelevelOrder.java
@@ -0,0 +1,76 @@
+
+/*
+ * 102. Binary Tree Level Order Traversal
+ * Medium
+ * Topics
+ * Companies
+ * Given the root of a binary tree, return the level order traversal of its
+ * nodes' values. (i.e., from left to right, level by level).
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: [[3],[9,20],[15,7]]
+ * Example 2:
+ * 
+ * Input: root = [1]
+ * Output: [[1]]
+ * Example 3:
+ * 
+ * Input: root = []
+ * Output: []
+ * 
+ */
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> levelNodes = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode curr = queue.poll();
+                levelNodes.add(curr.data);
+
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+            }
+
+            result.add(levelNodes);
+        }
+
+        return result;
+    }
+}
